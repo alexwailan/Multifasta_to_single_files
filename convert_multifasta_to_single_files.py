ab27aa015300fdb9bebf7a3e3e66a4305bc411b8
alexwailan <alex.wailan24@gmail.com>
2017-04-12 09:39:47 +0100

    slight_changes
    lol

--- convert_multifasta_to_single_files.py
+++ convert_multifasta_to_single_files.py
@@ -2,6 +2,6 @@
 inputData = []
 with open("./incFIIK_human_global_kp_multifasta.fa", "r") as data:
 	for line in data:
-		fastaLine= line.strip().split(">")
+		fastaLine= line.strip().split("\n")
 		print(fastaLine)
 		inputData.append(fastaLine)
