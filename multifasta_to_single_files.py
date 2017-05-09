80f8df79e35ae839027bbf98f6b83382a55af4d9
alexwailan <alex.wailan24@gmail.com>
2017-05-09 15:11:20 +0100

    option incrementvalue
    Adding an incremental value to the file name for cases of duplicates

--- multifasta_to_single_files.py
+++ multifasta_to_single_files.py
@@ -8,21 +8,29 @@ import argparse
 # input data
 parser = argparse.ArgumentParser(
 	description = 'Simply taking a multi fasta file of contigs and output each contig into seperate fasta files',
-	usage = 'Multifasta_to_Single [options] multifastafile')
-parser.add_argument('multifastafile', help='A Multi fasta file')
+	usage = 'Multifasta_to_Single [options] multifastafile unique_integer')
+parser.add_argument('multifastafile', help = 'A Multi fasta file as input')
+parser.add_argument('unique_integer', help = 'If you want a unique number per file out: input Yes or No ')
 options = parser.parse_args()
+output_integer = 0 #Counts the output; adds an incremental number to the file name
 
 opened = False #assume outfile is not open
 with open(options.multifastafile) as data:
-   
+    
     for line in data:
         if line[0] == ">": #if line starts with ">"
             if(opened): #Look at the end of loop; if previous contig file is open it will be close to start a new one
                 outfile.close() # Will close the outfile if it's open (look below and follow loop)
             opened = True #Set opened to true to represent an opened outfile
-            contig_name = line[1:].rstrip() #contig name; remove ">", extract contig string, remove any spaces or new lins following file
-            print('Contig file output: ' + contig_name)
-            outfile = open(str(contig_name) + ".fa", 'w')
+            if options.unique_integer == "Yes":
+                output_integer = output_integer + 1
+                contig_name = line[1:].rstrip() #contig name; remove ">", extract contig string, remove any spaces or new lins following file
+                outfile = open(str(contig_name) + "_" + str(output_integer) + ".fa", 'w') #output with a numberical integar for dataset
+            if options.unique_integer == "No":
+                contig_name = line[1:].rstrip() #contig name; remove ">", extract contig string, remove any spaces or new lins following file
+                outfile = open(str(contig_name) + ".fa", 'w')
+            print('Contig file output: ' + contig_name + ".fa")
+            
         outfile.write(line)
 
 outfile.close()          
